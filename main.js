9b51546d9ccd52cb4a3b52a934c097c8ba08843c
m0h1m3d <mohisham33@gmail.com>
2023-05-01 17:03:29 +0200

    edit code

--- main.js
+++ main.js
@@ -6,11 +6,26 @@ const btns = document.querySelectorAll('button');
 const playerSign = document.querySelector('.player-sign');
 const computerSign = document.querySelector('.cpu-sign');
 const result = document.querySelector('.result');
-const div = document.createElement('div');
-      div.classList.add('mob');
+const mob = document.querySelector('.mob');
+const re = document.querySelector('.reDiv')
+const restart = document.querySelector('.restart');
+const shadow = document.querySelector('.shadow');
 
 let playerScore = 0,
     computerScore = 0;
+
+playBtn.addEventListener('click', (e) => {
+  playerScore === 0;
+  computerScore === 0;
+
+  main.style.display = 'none';
+  main.style.opacity = '0';
+  game.style.display = 'block';
+  game.style.opacity = '1';
+  re.style.display = 'none';
+  playerPlay();
+});
+
 
 function playerPlay() {
   btns.forEach((button) => {
@@ -21,17 +36,18 @@ function playerPlay() {
         button.classList.remove('clicked');
       }, 200);
 
-      const playerSelection = button.id;
+      playerSelection = button.id;
       playerSign.textContent = button.id;
       console.log(`Player selected: ${playerSelection}`);
 
-      const computerSelection = cpuPlay();
+      computerSelection = cpuPlay();
       computerSign.textContent = computerSelection;
       console.log(`Computer selected: ${computerSelection}`);
 
       result.style.textAlign = 'center';
       result.style.fontSize = '1.2rem';
       result.style.height = '50%';
+      result.style.color = 'white';
 
       gameLogic(playerSelection, computerSelection);
       gameOver();
@@ -39,21 +55,45 @@ function playerPlay() {
   });
 }
 
+restart.addEventListener('click', (e) => {
+  restartGame();
+})
+
+
+function restartGame(){
+  playerScore === 0;
+  computerScore === 0;
+
+  window.location.reload();
+}
+
+
 function gameOver(){
-    if (playerScore === 5){
-        game.style.display = 'none';
-        main.style.display = 'none';
-        div.textContent = "Game Over! you win!";
-        div.style.color = 'green';
-        body.appendChild(div);
-    }
-    else if(computerScore === 5){
-        game.style.display = 'none';
-        main.style.display = 'none';
-        div.textContent = "Game Over! you lose!";
-        div.style.color = 'red';
-        body.appendChild(div);
-    }
+  if (playerScore === 5){
+    playerScore === 0;
+    computerScore === 0;
+
+      game.style.display = 'none';
+      main.style.display = 'none';
+      re.style.display = 'flex';
+      re.style.justifyContent = 'center';
+      re.style.alignItems = 'center';
+      mob.textContent = "Game Over! you win!";
+      mob.style.color = 'rgb(109, 233, 109)';
+  }
+  else if(computerScore === 5){
+    computerScore === 0;
+    playerScore === 0;
+
+      game.style.display = 'none';
+      main.style.display = 'none';
+      re.style.display = 'flex';
+      re.style.justifyContent = 'center';
+      re.style.alignItems = 'center';
+      mob.textContent = "Game Over! you lose!";
+      mob.style.color = 'rgb(255, 110, 110)';
+  }
+
 }
 
 const choices = ['✊🏻', '✋🏻', '✌🏼'];
@@ -64,27 +104,21 @@ function cpuPlay() {
 
 function gameLogic(playerSelection, computerSelection) {
   if (playerSelection === computerSelection) {
-    // console.log(`"It is a tie!. the score is:${playerScore}:${computerScore}"`);
-    result.textContent = `It is a tie!.\n the score is:${playerScore}:${computerScore}`;
+       result.textContent = `Tie! player score = ${playerScore}  computer score = ${computerScore}`;
   } else if (
     (playerSelection === '✊🏻' && computerSelection === '✌🏼') ||
     (playerSelection === '✋🏻' && computerSelection === '✊🏻') ||
     (playerSelection === '✌🏼' && computerSelection === '✋🏻')
   ) {
     playerScore++;
-    // console.log(`"You win this round!. the score is:${playerScore}:${computerScore}"`);
-    result.textContent = `You win this round!.\n the score is:${playerScore}:${computerScore}`;
+    result.textContent = `you win! player score = ${playerScore}  computer score = ${computerScore}`;
     
-  } else {
+  } else if(
+    (playerSelection === '✌🏼' && computerSelection === '✊🏻') ||
+    (playerSelection === '✋🏻' && computerSelection === '✌🏼') ||
+    (playerSelection === '✌🏼' && computerSelection === '✊🏻')
+  ) {
     computerScore++;
-    // console.log(`"computer wins this round!. the score is:${playerScore}:${computerScore}"`);
-    result.textContent = `computer wins this round!.\n the score is:${playerScore}:${computerScore}`;
+    result.textContent = `computer wins! player score = ${playerScore}  computer score = ${computerScore}`;
   }
 }
-
-playBtn.addEventListener('click', (e) => {
-  main.style.opacity = '0';
-  game.style.opacity = '1';
-  game.style.zIndex = '3';
-  playerPlay();
-});
